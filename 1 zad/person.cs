487a2a666b2addd5de0354c75d0eed3b081bfaa9
pramataroff <milenpramatarov@gmail.com>
2024-04-14 22:37:21 +0300

    first commit

---
+++ 1 zad/person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_zad
+{
+    public class Person
+    {
+        public string name;
+        public int age;
+        public void Eat()
+        {
+            Console.WriteLine(this.name + " papam");
+        }
+    }
+}
---
+++ zad 3/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad_3
+{
+    public class Person
+    {
+        public string name;
+        public int age;
+
+    }
+}
